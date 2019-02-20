db4b85e02bbc56f89c341ba64e5b6d4f1814ece9
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-02-20 11:09:35 +0700

    Sua Xung dot

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -17,32 +17,20 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> arrayls;
     EditText edt_NoiDung;
     ListView lvw_1;
-
-<<<<<<< HEAD
     int viTriChon=-1;
-
-
-=======
-    int viTriChon = -1;
->>>>>>> 3990d130ef18bed74add184858340a09e3394dad
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
+
+
         //Anh Xa
->>>>>>> 3990d130ef18bed74add184858340a09e3394dad
+
         AnhXa();
         ls = new ArrayList<String>();
         String[] some_array = getResources().getStringArray(R.array.mangListView);
         ls.addAll(Arrays.asList(some_array));
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 3990d130ef18bed74add184858340a09e3394dad
         arrayls = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,ls);
         lvw_1.setAdapter(arrayls);
         arrayls.notifyDataSetChanged();
@@ -54,41 +42,41 @@ public class MainActivity extends AppCompatActivity {
                 edt_NoiDung.setText(ls.get(i));
             }
         });
-<<<<<<< HEAD
 
-=======
->>>>>>> 3990d130ef18bed74add184858340a09e3394dad
+
     }
     void AnhXa(){
         edt_NoiDung = findViewById(R.id.edt_NoiDung);
         lvw_1 = findViewById(R.id.lvw_1);
     }
-<<<<<<< HEAD
+
     public void Sua(View view) {
-        if (viTriChon >= 0 ){
-            if (!edt_NoiDung.getText().toString().isEmpty()){
+        if (viTriChon >= 0) {
+            if (!edt_NoiDung.getText().toString().isEmpty()) {
                 String nameEdit = edt_NoiDung.getText().toString();
-                ls.set(viTriChon,nameEdit);
+                ls.set(viTriChon, nameEdit);
                 arrayls.notifyDataSetChanged();
-            }else {
-                Toast.makeText(MainActivity.this,"Khong duoc de trong noi dung",Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Khong duoc de trong noi dung", Toast.LENGTH_LONG).show();
             }
 
         } else {
-            Toast.makeText(MainActivity.this,"Moi ban chon ten muon sua",Toast.LENGTH_LONG).show();
-=======
+            Toast.makeText(MainActivity.this, "Moi ban chon ten muon sua", Toast.LENGTH_LONG).show();
 
-    public void Them(View view) {
-        String x = edt_NoiDung.getText().toString();
-        if (x.length()!=0){
-            ls.add(x);
-            arrayls.notifyDataSetChanged();
-
-
-        }else {
-            Toast.makeText(MainActivity.this,"Moi Ban Nhap Thong Tin Can Them",Toast.LENGTH_LONG).show();
-            edt_NoiDung.clearFocus();
->>>>>>> 3990d130ef18bed74add184858340a09e3394dad
         }
     }
-}
+        public void Them (View view){
+            String x = edt_NoiDung.getText().toString();
+            if (x.length() != 0) {
+                ls.add(x);
+                arrayls.notifyDataSetChanged();
+
+
+            } else {
+                Toast.makeText(MainActivity.this, "Moi Ban Nhap Thong Tin Can Them", Toast.LENGTH_LONG).show();
+                edt_NoiDung.clearFocus();
+
+            }
+        }
+    }
+
